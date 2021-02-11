b2a1412eb34d301f0b912e23a51e6322386ad607
Alp <alp@collectivat.cat>
2021-02-11 11:52:57 +0100

    gpu env

--- app/api/translateAPI.py
+++ app/api/translateAPI.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 #constants
 CONFIG_JSON_PATH = os.getenv('MT_API_CONFIG') 
 MODELS_ROOT_DIR = os.getenv('MODELS_ROOT')
-CTRANSLATE_DEVICE = os.getenv('MT_API_DEVICE') or 'cpu'
+CTRANSLATE_DEVICE = 'cuda' if os.getenv('MT_API_DEVICE')=='gpu' else 'cpu'
 CTRANSLATE_INTER_THREADS = int(os.getenv('MT_API_THREADS') or '16')
 MOSES_TOKENIZER_DEFAULT_LANG = 'en'
 
