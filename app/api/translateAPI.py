a78d05744ddcd132da44afd614569a6bee20f753
Alp <alp@collectivat.cat>
2021-03-12 17:44:03 +0100

    config error checks, sentencepiece model loading

--- app/api/translateAPI.py
+++ app/api/translateAPI.py
@@ -12,6 +12,7 @@ MODELS_ROOT_DIR = os.getenv('MODELS_ROOT')
 CTRANSLATE_DEVICE = 'cuda' if os.getenv('MT_API_DEVICE')=='gpu' else 'cpu'
 CTRANSLATE_INTER_THREADS = int(os.getenv('MT_API_THREADS') or '16')
 MOSES_TOKENIZER_DEFAULT_LANG = 'en'
+SUPPORTED_MODEL_TYPES = ['opus', 'ctranslator2', 'dummy']
 
 translate = APIRouter()
 
@@ -50,7 +51,6 @@ def parse_model_id(model_id):
 
     return src, tgt, alt
 
-
 def get_ctranslator(ctranslator_model_path):
     from ctranslate2 import Translator
     ctranslator = Translator(ctranslator_model_path)
@@ -66,39 +66,50 @@ def get_batch_ctranslator(ctranslator_model_path):
 def get_batch_opustranslator(src, tgt):   
     from transformers import MarianTokenizer, MarianMTModel
     model_name = f'Helsinki-NLP/opus-mt-{src}-{tgt}'
-    tokenizer = MarianTokenizer.from_pretrained(model_name)
-    model = MarianMTModel.from_pretrained(model_name)
+    try:
+        tokenizer = MarianTokenizer.from_pretrained(model_name)
+        model = MarianMTModel.from_pretrained(model_name)
 
-    translator = lambda x: tokenizer.batch_decode(model.generate(**tokenizer.prepare_seq2seq_batch(src_texts=x, return_tensors="pt")), skip_special_tokens=True) if x else ""
+        translator = lambda x: tokenizer.batch_decode(model.generate(**tokenizer.prepare_seq2seq_batch(src_texts=x, return_tensors="pt")), skip_special_tokens=True) if x else ""
+    except:
+        translator = None
+
     return translator
 
 def get_moses_tokenizer(lang):
     from sacremoses import MosesTokenizer
-    try:
-        moses_tokenizer = MosesTokenizer(lang=lang)
-    except:
-        print("WARNING: Moses doesn't have tokenizer for", lang)
-        moses_tokenizer = MosesTokenizer(lang=MOSES_TOKENIZER_DEFAULT_LANG)
-        
+    moses_tokenizer = MosesTokenizer(lang=lang)
     tokenizer = lambda x: moses_tokenizer.tokenize(x, return_str=True) #string IN -> string OUT
+    return tokenizer
+
+def get_moses_detokenizer(lang):
+    from sacremoses import  MosesDetokenizer
+    moses_detokenizer = MosesDetokenizer(lang=lang)
+    tokenizer = lambda x: moses_detokenizer.detokenize(x.split(), return_str=True) #string IN -> string OUT
     return tokenizer
 
 def get_bpe_segmenter(bpe_codes_path):
     from subword_nmt import apply_bpe
-    bpe = apply_bpe.BPE(codes=open(bpe_codes_path, 'r'))
-    segmenter = lambda x: bpe.process_line(x.strip()).split() #string IN -> list OUT
+    try:
+        bpe = apply_bpe.BPE(codes=open(bpe_codes_path, 'r'))
+        segmenter = lambda x: bpe.process_line(x.strip()).split() #string IN -> list OUT
+        return segmenter
+    except Exception as e:
+        return None
+
+def get_sentencepiece_segmenter(sp_model_path):
+    import sentencepiece as spm
+    sp = spm.SentencePieceProcessor()
+    sp.load(sp_model_path)
+    segmenter = lambda x: sp.encode_as_pieces(x) #string IN -> list OUT
     return segmenter
 
-def get_moses_detokenizer(lang):
-    from sacremoses import  MosesDetokenizer
-    try:
-        moses_detokenizer = MosesDetokenizer(lang=lang)
-    except:
-        print("WARNING: Moses doesn't have tokenizer for", lang)
-        moses_detokenizer = MosesDetokenizer(lang=MOSES_TOKENIZER_DEFAULT_LANG)
-        
-    tokenizer = lambda x: moses_detokenizer.detokenize(x.split(), return_str=True) #string IN -> string OUT
-    return tokenizer
+def get_sentencepiece_desegmenter(sp_model_path):
+    import sentencepiece as spm
+    sp = spm.SentencePieceProcessor()
+    sp.load(sp_model_path)
+    desentencepiece = lambda x: sp.decode_pieces(x) # list IN -> string OUT
+    return desentencepiece
 
 def tokenize_with_punkset(doc, punkset):
     tokens = []
@@ -121,118 +132,266 @@ def tokenize_with_punkset(doc, punkset):
 def get_custom_tokenizer(punkset):
     return lambda x: tokenize_with_punkset(x, punkset)
 
-
 def do_translate(model_id, text):
+    print("do_translate")
+    print(text)
+
     if model_id in loaded_models:
         if loaded_models[model_id]['sentence_segmenter']:
             sentence_batch = loaded_models[model_id]['sentence_segmenter'](text)
         else:
             sentence_batch = [text]
 
-        #print(sentence_batch)
+        print("sentence_batch")
+        print(sentence_batch)
 
         #preprocess
-        # print("Preprocess")
+        print("Preprocess")
         for step in loaded_models[model_id]['preprocessors']:
+            print(step)
             sentence_batch = [step(s) for s in sentence_batch]
             print(sentence_batch)
 
         #translate batch (ctranslate only)
-        translated_sentence_batch = loaded_models[model_id]['translator'](sentence_batch)
-        # print(translated_sentence_batch)
+        if loaded_models[model_id]['translator']:
+            translated_sentence_batch = loaded_models[model_id]['translator'](sentence_batch)
+            print("translated_sentence_batch")
+            print(translated_sentence_batch)
+        else:
+            translated_sentence_batch = sentence_batch
 
         #postprocess
-        #print("Postprocess")
+        print("Postprocess")
         tgt_sentences = translated_sentence_batch
         for step in loaded_models[model_id]['postprocessors']:
             tgt_sentences = [step(s) for s in tgt_sentences]
-            #print(tgt_sentences)
+            print(tgt_sentences)
 
         tgt_text = " ".join(tgt_sentences)
+        print("tgt_text")
+        print(tgt_text)
 
         return tgt_text
     else:
         return 0
 
 #Functional
-def read_config(config_file):
-    with open(config_file, "r") as jsonfile: 
-        data = json.load(jsonfile) 
-        print("Config Read successful") 
-        print(data)
-    return data
+# def read_config(config_file):
+#     with open(config_file, "r") as jsonfile: 
+#         data = json.load(jsonfile) 
+#         print("Config Read successful") 
+#         print(data)
+#     return data
 
 def load_models(config_path):
-    config_data = read_config(CONFIG_JSON_PATH)
-    global language_codes
-    language_codes = config_data['languages']
-    for model_config in config_data['models']:
-        if model_config['load']:
+    #Check if config file is there and well formatted
+    if not os.path.exists(CONFIG_JSON_PATH):
+        print("WARNING: Config file %s not found. No models will be loaded."%CONFIG_JSON_PATH)
+        return 0
 
+    try:
+        with open(CONFIG_JSON_PATH, "r") as jsonfile: 
+            config_data = json.load(jsonfile)
+    except:
+        print("ERROR: Config file format broken. No models will be loaded.")
+        return 0
+
+    #Check if MODELS_ROOT_DIR exists
+    if not os.path.exists(MODELS_ROOT_DIR):
+        print("ERROR: models directory not found. No models will be loaded.")
+        return 0
+
+    if 'languages' in config_data:
+        global language_codes
+        language_codes = config_data['languages']
+        print("Languages: %s"%language_codes)
+    else:
+        print("WARNING: Language name spefication dictionary ('languages') not found in configuration." )
+
+    if not 'models' in config_data:
+        print("ERROR: Model spefication list ('models') not found in configuration." )
+        return 0
+
+    for model_config in config_data['models']:
+        if not 'load' in model_config or model_config['load']:
+            #CONFIG CHECKS
+            #Check if model_type src and tgt fields are specified
+            if not 'src' in model_config:
+                print("WARNING: Source language (src) not speficied for a model. Skipping load")
+                continue
+
+            if not 'tgt' in model_config:
+                print("WARNING: Target language (tgt) not speficied for a model. Skipping load")
+                continue
+
+            if not 'model_type' in model_config:
+                print("WARNING: model_type not speficied for model. Skipping load")
+                continue
+
+            if not model_config['model_type'] in SUPPORTED_MODEL_TYPES:
+                print("WARNING: model_type not recognized: %s. Skipping load"%model_config['model_type'])
+                continue
+
+            #Load model variables
+            model = {}
+            model['src'] = model_config['src']
+            model['tgt'] = model_config['tgt']
+            
             if 'alt' in model_config:
                 alt_id = model_config['alt']
             else:
                 alt_id = None
-            
+
             model_id = get_model_id(model_config['src'], model_config['tgt'], alt_id)
-            loaded_models[model_id] = {}
-            loaded_models[model_id]['src'] = model_config['src']
-            loaded_models[model_id]['tgt'] = model_config['tgt']
-            
-            #Load sentence segmenter
-            if model_config['sentence_split'] == "nltk":
-                loaded_models[model_id]['sentence_segmenter'] = nltk_sentence_segmenter
-            elif type(model_config['sentence_split']) == list:
-                loaded_models[model_id]['sentence_segmenter'] = get_custom_tokenizer(model_config['sentence_split'])
+
+            #Check if language names exist for the language ids
+            if not model['src'] in language_codes:
+                print("WARNING: Source language code %s not defined in languages dict. This will surely break something."%model['src'])
+            if not model['tgt'] in language_codes:
+                print("WARNING: Target language code %s not defined in languages dict. This will surely break something."%model['tgt'])
+
+            #Check model path
+            if 'model_path' in model_config and model_config['model_path']:
+                model_dir = os.path.join(MODELS_ROOT_DIR, model_config['model_path'])
+                if not os.path.exists(model_dir):
+                    print("WARNING: Model path %s not found for model %s. Can't load custom translation model or segmenters."%(model_dir, model_id))
+                    model_dir = None
             else:
-                loaded_models[model_id]['sentence_segmenter'] = None
-            
+                print("WARNING: Model path not specified. Can't load custom translation model or segmenters.")
+                model_dir = None
+
+            #More configuration checks
+            #Check conflicting subword segmenters
+            if 'bpe' in model_config['pipeline'] and 'sentencepiece' in model_config['pipeline']:
+                print("WARNING: Model %s has both sentencepiece and bpe setup. "%model_id)
+
+            #Check conflicting model ids
+            if model_id in loaded_models:
+                print("WARNING: Overwriting model %s since there are duplicate entries. Make sure you give an 'alt' ids to load alternate models."%model_id)
+
+
             #Load model pipeline
-            print("Model: %s ( "%model_id, end="")
-            loaded_models[model_id]['preprocessors'] = []
+            print("Model: %s ("%model_id, end=" ")
+
+            #Load sentence segmenter
+            if 'sentence_split' in model_config:
+                if model_config['sentence_split'] == "nltk":
+                    print("sentence_split-nltk", end=" ")
+                    model['sentence_segmenter'] = nltk_sentence_segmenter
+                elif type(model_config['sentence_split']) == list:
+                    print("sentence_split-custom", end=" ")
+                    model['sentence_segmenter'] = get_custom_tokenizer(model_config['sentence_split'])
+                else:
+                    model['sentence_segmenter'] = None
+            else:
+                model['sentence_segmenter'] = None
+
+            #Load pre/post-processors
+            model['preprocessors'] = []
+            model['postprocessors'] = []
+            bpe_ok = False
+            sentencepiece_ok = False
+
             if 'lowercase' in model_config['pipeline'] and model_config['pipeline']['lowercase']:
+                model['preprocessors'].append(lowercaser)
                 print("lowercase", end=" ")
-                loaded_models[model_id]['preprocessors'].append(lowercaser)
 
             if 'tokenize' in model_config['pipeline'] and model_config['pipeline']['tokenize']:
-                print("tokenize", end=" ")
-                loaded_models[model_id]['preprocessors'].append(get_moses_tokenizer(model_config['src']))
+                tokenizer = get_moses_tokenizer(model_config['src'])
+                model['preprocessors'].append(tokenizer)
+                print("mtokenize", end=" ")
 
-            if 'bpe' in model_config['pipeline'] and model_config['pipeline']['bpe']:
+            if model_dir and 'bpe' in model_config['pipeline'] and model_config['pipeline']['bpe']:
+                if not 'bpe_file' in model_config:
+                    print("\nWARNING: Failed to load bpe model for %s: bpe_file not specified. Skipping load."%(model_id))
+                    continue
+
+                model_file = os.path.join(model_dir, model_config['bpe_file'])
+
+                if not os.path.exists(model_file):
+                    print("\nWARNING: Failed to load bpe model for %s: BPE vocabulary file not found at %s. Skipping load."%(model_id, model_file))
+                    continue
+
+                bpe_segmenter = get_bpe_segmenter(model_file)
+
+                if not bpe_segmenter:
+                    print("\nWARNING: Failed to loading bpe model %s for %s. Skipping load."%(model_file, model_id))
+                    continue
+
+                model['preprocessors'].append(get_bpe_segmenter(model_file))
+                bpe_ok = True
                 print("bpe", end=" ")
-                model_dir = os.path.join(MODELS_ROOT_DIR, model_config['model_path'])
-                loaded_models[model_id]['preprocessors'].append(get_bpe_segmenter(os.path.join(model_dir, model_config['bpe_file'])))
-            elif model_config['model_type'] == 'ctranslator2':
-                loaded_models[model_id]['preprocessors'].append(token_segmenter)
+            elif model_dir and 'sentencepiece' in model_config['pipeline'] and model_config['pipeline']['sentencepiece']:
+                if not model_dir:
+                    print("\nWARNING: Failed to load sentencepiece model for %s: model_path not specified. Skipping load."%(model_id))
+                    continue
 
+                if not 'sentencepiece_model' in model_config:
+                    print("\nWARNING: Failed to load sentencepiece model for %s: sentencepiece_model not specified. Skipping load."%(model_id))
+                    continue
+
+                model_file = os.path.join(model_dir, model_config['sentencepiece_model'])
+                if not os.path.exists(model_file):
+                    print("\nWARNING: Failed to load sentencepiece model for %s: Sentencepiece model file not found at %s. Skipping load."%(model_id, model_file))
+                    continue
+
+                model['preprocessors'].append(get_sentencepiece_segmenter(model_file))
+                sentencepiece_ok = True
+                print("sentencepiece", end=" ")
+            elif model_config['model_type'] == 'ctranslator2':
+                #default tokenizer needed for ctranslator2 translation
+                model['preprocessors'].append(token_segmenter)
+
+            
             if 'translate' in model_config['pipeline'] and model_config['pipeline']['translate']:
                 print("translate", end="")
                 if model_config['model_type'] == 'ctranslator2':
-                    print("-ctranslator2", end=" ")
-                    model_dir = os.path.join(MODELS_ROOT_DIR, model_config['model_path'])
-                    loaded_models[model_id]['translator'] = get_batch_ctranslator(model_dir)  
-                elif model_config['model_type'] == 'opus':
-                    loaded_models[model_id]['translator'] = get_batch_opustranslator(loaded_models[model_id]['src'], loaded_models[model_id]['tgt'])
-                    print("-opus-huggingface", end=" ")
-            else:
-                loaded_models[model_id]['translator'] = dummy_translator
+                    if not model_dir:
+                        print("\nWARNING: Failed to load ctranslate model for %s: model_path not specified. Skipping load."%(model_id))
+                        continue
 
-            loaded_models[model_id]['postprocessors'] = []
-            if 'bpe' in model_config['pipeline'] and model_config['pipeline']['bpe']:
+                    model['translator'] = get_batch_ctranslator(model_dir)  
+                    print("-ctranslator2", end=" ") 
+                elif model_config['model_type'] == 'opus':
+                    opus_translator = get_batch_opustranslator(model['src'], model['tgt'])
+                    if opus_translator:
+                        model['translator'] = get_batch_opustranslator(model['src'], model['tgt'])
+                        print("-opus-huggingface", end=" ")
+                    else: 
+                        print("\nWARNING: Failed to load opus-huggingface model for %s. Skipping load."%(model_id))
+                        continue
+                elif model_config['model_type'] == 'dummy':
+                    print("-dummy", end=" ")
+                    model['translator'] = dummy_translator
+            else:
+                model['translator'] = None
+
+            if bpe_ok:
+                model['postprocessors'].append(desegmenter)
                 print("unbpe", end=" ")
-                loaded_models[model_id]['postprocessors'].append(desegmenter)
+            elif sentencepiece_ok:
+                model_file = os.path.join(model_dir, model_config['sentencepiece_model'])
+                model['postprocessors'].append(get_sentencepiece_desegmenter(model_file))
+                print("desentencepiece", end=" ")
             elif model_config['model_type'] == 'ctranslator2':
-                loaded_models[model_id]['postprocessors'].append(token_desegmenter)
+                model['postprocessors'].append(token_desegmenter)
 
             if 'tokenize' in model_config['pipeline'] and model_config['pipeline']['tokenize']:
-                print("detokenize", end=" ")
-                loaded_models[model_id]['postprocessors'].append(get_moses_detokenizer(model_config['tgt']))
+                detokenizer = get_moses_detokenizer(model_config['tgt'])
+                model['postprocessors'].append(detokenizer)
+                print("mdetokenize", end=" ")
 
             if 'recase' in model_config['pipeline'] and model_config['pipeline']['recase']:
+                model['postprocessors'].append(capitalizer)
                 print("recase", end=" ")
-                loaded_models[model_id]['postprocessors'].append(capitalizer)
 
             print(")")
+
+            #All good, add model to the list
+            loaded_models[model_id] = model
+        
+    return 1
+
     
 #HTTP operations
 class TranslationRequest(BaseModel):
@@ -291,7 +450,6 @@ async def translate_batch(request: BatchTranslationRequest):
 @translate.get('/languages', status_code=200)
 async def languages():
     languages_list = {}
-    print(language_codes)
     for model_id in loaded_models.keys():
         source, target, alt = parse_model_id(model_id)
         if not source in languages_list:
